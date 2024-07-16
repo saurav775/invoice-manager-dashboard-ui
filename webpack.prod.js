4373b8165f0f30caab7cf1b57db30eb77704daab
saurav775 <sauravchoudhury775@gmail.com>
2024-07-16 13:34:34 +0530

    fix: webpack prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -18,8 +18,18 @@ module.exports = () => {
         name: "invoice_manager_dashboard_ui",
         filename: "remoteEntry.js",
         remotes: {
+          invoice_manager_dashboard_ui:
+            "invoice_manager_dashboard_ui@http://localhost:3000/remoteEntry.js",
           invoice_manager_customer_ui:
-            `invoice_manager_customer_ui@${domain}/remoteEntry.js`,
+            "invoice_manager_customer_ui@http://localhost:3001/remoteEntry.js",
+          invoice_manager_product_ui:
+            "invoice_manager_product_ui@http://localhost:3002/remoteEntry.js",
+          invoice_manager_invoice_ui:
+            "invoice_manager_invoice_ui@http://localhost:3003/remoteEntry.js",
+        },
+        exposes: {
+          "./store": "./src/store",
+          "./actionTypes": "./src/store/actionTypes",
         },
         shared: packageJson.dependencies,
       }),
